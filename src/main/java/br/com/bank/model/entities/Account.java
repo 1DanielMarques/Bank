81459909110f7a249a9780309544cb8f16577d1c
mDanielSE <daniel.marques@edu.unifil.br>
2022-10-22 19:01:47 -0300

    creating Account class

--- src/main/java/br/com/bank/model/entities/Account.java
+++ src/main/java/br/com/bank/model/entities/Account.java
@@ -2,6 +2,24 @@ package br.com.bank.model.entities;
 
 public class Account {
 
+    private int number;
+    private double balance;
 
+    public Account(int number, double balance) {
+        this.number = number;
+        this.balance = balance;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
 
 }
