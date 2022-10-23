c7196839fcfdfe935db2066183ad554944c2a6f5
mDanielSE <daniel.marques@edu.unifil.br>
2022-10-23 14:01:11 -0300

    IAccountOperations interface

---
+++ src/main/java/br/com/bank/model/services/IAccountOperations.java
@@ -0,0 +1,12 @@
+package br.com.bank.model.services;
+
+import br.com.bank.model.entities.Account;
+
+public interface IAccountOperations {
+
+    void deposit(Account account, double amount);
+    void withdraw(double amount);
+    void transfer(Account account, double amount);
+
+
+}
