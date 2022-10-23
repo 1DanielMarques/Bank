c162c5bc1311299628f60612f64bbffb2e1b60a7
mDanielSE <daniel.marques@edu.unifil.br>
2022-10-23 14:08:02 -0300

    Implementing operation methods in Account

--- src/main/java/br/com/bank/model/entities/Account.java
+++ src/main/java/br/com/bank/model/entities/Account.java
@@ -1,13 +1,18 @@
 package br.com.bank.model.entities;
 
+import br.com.bank.model.services.IAccountOperations;
+
 public class Account {
 
     private int number;
     private double balance;
 
-    public Account(int number, double balance) {
+    private IAccountOperations operations;
+
+    public Account(int number, double balance, IAccountOperations operations) {
         this.number = number;
         this.balance = balance;
+        this.operations = operations;
     }
 
     public int getNumber() {
@@ -24,5 +29,17 @@ public class Account {
 
     public void setBalance(double balance) {
         this.balance = balance;
+    }
+
+    public void deposit(Account account, double amount) {
+        operations.deposit(account, amount);
+    }
+
+    public void withdraw(Account account, double amount) {
+        operations.withdraw(account, amount);
+    }
+
+    public void transfer(double amount, Account other) {
+        operations.transfer(this, amount, other);
     }
 }
