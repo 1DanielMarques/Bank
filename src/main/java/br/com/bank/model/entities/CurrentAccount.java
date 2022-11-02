7d69acd196376b551778269391438e0fc1fcd75f
daniel <daniel.marques@edu.unifil.br>
2022-11-02 19:37:09 -0300

    creating CurrentAccount

---
+++ src/main/java/br/com/bank/model/entities/CurrentAccount.java
@@ -0,0 +1,7 @@
+package br.com.bank.model.entities;
+
+public class CurrentAccount extends Account{
+    public CurrentAccount(int number, double balance) {
+        super(number, balance);
+    }
+}
