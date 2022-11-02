5af6fb7eb27affd76db5a2b983c7d7c99b1f251f
daniel <daniel.marques@edu.unifil.br>
2022-11-02 19:37:38 -0300

    creating CurrentAccountOp

---
+++ src/main/java/br/com/bank/model/services/operations/CurrentAccountOp.java
@@ -0,0 +1,20 @@
+package br.com.bank.model.services.operations;
+
+import br.com.bank.model.entities.Account;
+
+public class CurrentAccountOp implements IAccountOperations{
+    @Override
+    public void deposit(Account account, double amount) {
+
+    }
+
+    @Override
+    public void withdraw(Account account, double amount) {
+
+    }
+
+    @Override
+    public void transfer(Account account, double amount, Account other) {
+
+    }
+}
